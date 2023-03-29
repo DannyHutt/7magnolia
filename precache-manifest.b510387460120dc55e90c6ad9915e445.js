add261d61b2ed26c8bdabd6c287e748c06493ced
Dan Gauthier <DannyHutt@users.noreply.github.com>
2023-03-29 00:29:09 -0400

    Updates

---
+++ precache-manifest.b510387460120dc55e90c6ad9915e445.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb27c54651e750d64842",
+    "url": "/7magnolia/static/css/main.4dcb57e8.chunk.css"
+  },
+  {
+    "revision": "bb27c54651e750d64842",
+    "url": "/7magnolia/static/js/main.0ddc82ae.chunk.js"
+  },
+  {
+    "revision": "cd0bc2ac02638b866131",
+    "url": "/7magnolia/static/js/runtime~main.7f004e70.js"
+  },
+  {
+    "revision": "470043ee201461ab2280",
+    "url": "/7magnolia/static/js/2.d095bb04.chunk.js"
+  },
+  {
+    "revision": "77da778a10b57eb72fd7",
+    "url": "/7magnolia/static/js/3.5d65379c.chunk.js"
+  },
+  {
+    "revision": "6667e33b019d777020e994ef8bab97c7",
+    "url": "/7magnolia/static/media/tempAvatar.6667e33b.png"
+  },
+  {
+    "revision": "f5fc5e60175a43bc2116710ef82d185c",
+    "url": "/7magnolia/static/media/linkedin-icon.f5fc5e60.svg"
+  },
+  {
+    "revision": "6da749c06225e64e62415a3f13a8c333",
+    "url": "/7magnolia/static/media/logo.6da749c0.svg"
+  },
+  {
+    "revision": "a42d6f939a403eccc5fe22145bf9c87d",
+    "url": "/7magnolia/static/media/7m.a42d6f93.svg"
+  },
+  {
+    "revision": "76b1c4a29387a4ccdad61f8e96149521",
+    "url": "/7magnolia/static/media/hero-overlay.76b1c4a2.svg"
+  },
+  {
+    "revision": "025812793db0e5f31033347ad2a52a08",
+    "url": "/7magnolia/static/media/development-icon.02581279.svg"
+  },
+  {
+    "revision": "e4ea2e849064cb9a5c624f7cec19ca7c",
+    "url": "/7magnolia/static/media/ux-icon.e4ea2e84.svg"
+  },
+  {
+    "revision": "882a6218bc53f675bf324ef166065f6c",
+    "url": "/7magnolia/static/media/visual-design-icon.882a6218.svg"
+  },
+  {
+    "revision": "d166ced622cca73d79f8971c6f75e1ae",
+    "url": "/7magnolia/static/media/plus-icon.d166ced6.svg"
+  },
+  {
+    "revision": "eb48cc30218e15d2ed41ef21c53db76a",
+    "url": "/7magnolia/static/media/pig-icon.eb48cc30.svg"
+  },
+  {
+    "revision": "7a00094d79e625c736cc0c5d3bfa4ce2",
+    "url": "/7magnolia/static/media/tech-icon.7a00094d.svg"
+  },
+  {
+    "revision": "607e7ac3a7f2e17c8050b190a11e54b6",
+    "url": "/7magnolia/static/media/education-icon.607e7ac3.svg"
+  },
+  {
+    "revision": "883d79cb7c5c3d76ca910d141e3fb51d",
+    "url": "/7magnolia/static/media/health-icon.883d79cb.svg"
+  },
+  {
+    "revision": "399809a7635a16c6cdf6dba3875632a8",
+    "url": "/7magnolia/static/media/partner-icon.399809a7.svg"
+  },
+  {
+    "revision": "65675256bb77cf6dca8c3688c7e0f4ab",
+    "url": "/7magnolia/static/media/hands-icon.65675256.svg"
+  },
+  {
+    "revision": "18c2a00cadd770b536a209cd3652e31d",
+    "url": "/7magnolia/static/media/flower.18c2a00c.png"
+  },
+  {
+    "revision": "56e5894a8ee88bc90fc5d4ba611a7e6d",
+    "url": "/7magnolia/index.html"
+  }
+];
