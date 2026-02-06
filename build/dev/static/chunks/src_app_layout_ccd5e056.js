dccfcfbd3553c0f03d456fca0f146c7b49600862
Dan Gauthier <DannyHutt@users.noreply.github.com>
2026-02-06 11:20:41 -0500

    removing build folder.

--- build/dev/static/chunks/src_app_layout_ccd5e056.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_c0d369b5._.css",
-  "static/chunks/node_modules_cd0ae1d9._.js",
-  "static/chunks/src_fac7fc30._.js"
-],
-    source: "dynamic"
-});
--- build/dev/static/chunks/src_app_page_67592bf3.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_Components_76517f58._.css",
-  "static/chunks/src_dee3f0ef._.js"
-],
-    source: "dynamic"
-});
